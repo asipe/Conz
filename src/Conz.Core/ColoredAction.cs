4b1a271bb7f45d019e9b37a0fddfab8f0fc05cb0
Andy Sipe <ajs.general@gmail.com>
2014-08-07 09:07:15 -0400

    added ColoredAction class

---
+++ src/Conz.Core/ColoredAction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Conz.Core.ConsoleAbstraction;
+
+namespace Conz.Core {
+  public class ColoredAction {
+    public ColoredAction(IConsole console, ConsoleColor foregroundColor) {
+      mConsole = console;
+      mForegroundColor = foregroundColor;
+    }
+
+    public void Execute(Action<IConsole> action) {
+      var originalForegroundColor = mConsole.ForegroundColor;
+      mConsole.ForegroundColor = mForegroundColor;
+      try {
+        action.Invoke(mConsole);
+      } finally {
+        mConsole.ForegroundColor = originalForegroundColor;
+      }
+    }
+
+    private readonly IConsole mConsole;
+    private readonly ConsoleColor mForegroundColor;
+  }
+}
---
+++ src/Conz.UnitTests/Core/ColoredActionTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Conz.Core;
+using Conz.Core.ConsoleAbstraction;
+using Moq;
+using NUnit.Framework;
+
+namespace Conz.UnitTests.Core {
+  [TestFixture]
+  public class ColoredActionTest : BaseTestCase {
+    [Test]
+    public void TestSuccessfulAction() {
+      mConsole.Setup(c => c.ForegroundColor).Returns(ConsoleColor.Green);
+      mConsole.SetupSet(c => c.ForegroundColor = ConsoleColor.Red);
+      mConsole.SetupSet(c => c.ForegroundColor = ConsoleColor.Green);
+      IConsole console = null;
+      mAction.Execute(c => console = c);
+      Assert.That(console, Is.EqualTo(mConsole.Object));
+    }
+
+    [Test]
+    public void TestActionThrowsStillResetsColor() {
+      mConsole.Setup(c => c.ForegroundColor).Returns(ConsoleColor.Green);
+      mConsole.SetupSet(c => c.ForegroundColor = ConsoleColor.Red);
+      mConsole.SetupSet(c => c.ForegroundColor = ConsoleColor.Green);
+      IConsole console = null;
+      var ex = Assert.Throws<Exception>(() => mAction.Execute(c => {
+                                                                console = c;
+                                                                throw new Exception("Test Error");
+                                                              }));
+      Assert.That(ex.Message, Is.EqualTo("Test Error"));
+      Assert.That(console, Is.EqualTo(mConsole.Object));
+    }
+
+    [SetUp]
+    public void DoSetup() {
+      mConsole = Mok<IConsole>();
+      mAction = new ColoredAction(mConsole.Object, ConsoleColor.Red);
+    }
+
+    private Mock<IConsole> mConsole;
+    private ColoredAction mAction;
+  }
+}
