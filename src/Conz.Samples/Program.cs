11e477e6a71d423324d0c5904c66efc6fb5833ab
Andy Sipe <ajs.general@gmail.com>
2014-08-07 09:20:24 -0400

    added Conz.Samples project

---
+++ src/Conz.Samples/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Conz.Core;
+using Conz.Core.ConsoleAbstraction;
+
+namespace Conz.Samples {
+  internal class Program {
+    private static void Main() {
+      try {
+        DoWork();
+      } catch (Exception e) {
+        Console.WriteLine("e: {0}", e);
+      }
+    }
+
+    private static void DoWork() {
+      var console = new DotNetConsole();
+
+      var conzoles = Enum
+        .GetValues(typeof(ConsoleColor))
+        .Cast<ConsoleColor>()
+        .Select(color => new Conzole(console, new ConzoleConfig {ForegroundColor = color}))
+        .ToArray();
+
+      Array.ForEach(conzoles, con => con.WriteLine("Hello World"));
+    }
+  }
+}
---
+++ src/Conz.Samples/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Conz.Samples")]
+[assembly: AssemblyDescription("Conz.Samples")]
+[assembly: AssemblyProduct("Conz.Samples")]
+[assembly: Guid("16aab726-fbbe-4701-a505-920ff3273842")]
