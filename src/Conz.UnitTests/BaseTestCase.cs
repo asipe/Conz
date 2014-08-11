c2dc106f26c00f48600482f1bd8ce7e513e8b25b
Andy Sipe <ajs.general@gmail.com>
2014-08-11 07:58:46 -0400

    integrated autofixture

--- src/Conz.UnitTests/BaseTestCase.cs
+++ src/Conz.UnitTests/BaseTestCase.cs
@@ -1,5 +1,7 @@
-﻿using Moq;
+﻿using System.Linq;
+using Moq;
 using NUnit.Framework;
+using Ploeh.AutoFixture;
 
 namespace Conz.UnitTests {
   [TestFixture]
@@ -7,6 +9,7 @@ namespace Conz.UnitTests {
     [SetUp]
     public void BaseSetup() {
       MokFac = new MockRepository(MockBehavior.Strict);
+      ObjectFixture = new Fixture();
     }
 
     [TearDown]
@@ -22,10 +25,23 @@ namespace Conz.UnitTests {
       return items;
     }
 
+    protected T CA<T>() {
+      return ObjectFixture.Create<T>();
+    }
+
+    protected T[] CM<T>(int count) {
+      return (count == 0)
+               ? BA<T>()
+               : ObjectFixture
+                   .CreateMany<T>(count)
+                   .ToArray();
+    }
+
     private void VerifyMocks() {
       MokFac.VerifyAll();
     }
 
     protected MockRepository MokFac{get;private set;}
+    protected Fixture ObjectFixture{get;private set;}
   }
 }
--- src/Conz.UnitTests/Core/StyleSheetTest.cs
+++ src/Conz.UnitTests/Core/StyleSheetTest.cs
@@ -6,8 +6,8 @@ namespace Conz.UnitTests.Core {
   public class StyleSheetTest : BaseTestCase {
     [Test]
     public void TestDefaults() {
-      var def = new Style(null, null, null);
-      var styles = new Style[3];
+      var def = CA<Style>();
+      var styles = CM<Style>(3);
       var sheet = new StyleSheet(def, styles);
       Assert.That(sheet.Default, Is.EqualTo(def));
       Assert.That(sheet.Styles, Is.EqualTo(styles));
