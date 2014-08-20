8f37887494ae277cc793d65dc5493fff69ff6d95
Andy Sipe <ajs.general@gmail.com>
2014-08-20 08:01:51 -0400

    added NotImplemented versions of the various IConsole methods

--- src/Conz.Core/Conzole.cs
+++ src/Conz.Core/Conzole.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.IO;
 using Conz.Core.ConsoleAbstraction;
 
 namespace Conz.Core {
-  public class Conzole {
+  public class Conzole : IConsole {
     public Conzole(StyleSheet styleSheet) : this(new DotNetConsole(), new Parser(), new ColoredActionFactory(), styleSheet) {}
 
     public Conzole(IConsole console,
@@ -16,13 +17,191 @@ namespace Conz.Core {
       mStyleSheet = styleSheet;
     }
 
+    public TextWriter Error {
+      get {throw new NotImplementedException();}
+    }
+
+    public TextReader In {
+      get {throw new NotImplementedException();}
+    }
+
+    public TextWriter Out {
+      get {throw new NotImplementedException();}
+    }
+
+    public ConsoleColor BackgroundColor {
+      get {throw new NotImplementedException();}
+      set {throw new NotImplementedException();}
+    }
+
+    public ConsoleColor ForegroundColor {
+      get {throw new NotImplementedException();}
+      set {throw new NotImplementedException();}
+    }
+
+    public void WriteLine() {
+      throw new NotImplementedException();
+    }
+
+    public void WriteLine(bool value) {
+      throw new NotImplementedException();
+    }
+
+    public void WriteLine(char value) {
+      throw new NotImplementedException();
+    }
+
+    public void WriteLine(char[] buffer) {
+      throw new NotImplementedException();
+    }
+
+    public void WriteLine(decimal value) {
+      throw new NotImplementedException();
+    }
+
+    public void WriteLine(double value) {
+      throw new NotImplementedException();
+    }
+
+    public void WriteLine(int value) {
+      throw new NotImplementedException();
+    }
+
+    public void WriteLine(long value) {
+      throw new NotImplementedException();
+    }
+
+    public void WriteLine(object value) {
+      throw new NotImplementedException();
+    }
+
+    public void WriteLine(float value) {
+      throw new NotImplementedException();
+    }
+
     public void WriteLine(string value) {
       Write(value);
       mConsole.WriteLine();
     }
 
+    public void WriteLine(uint value) {
+      throw new NotImplementedException();
+    }
+
+    public void WriteLine(ulong value) {
+      throw new NotImplementedException();
+    }
+
+    public void WriteLine(string format, object arg0) {
+      throw new NotImplementedException();
+    }
+
+    public void WriteLine(string format, params object[] arg) {
+      throw new NotImplementedException();
+    }
+
+    public void WriteLine(char[] buffer, int index, int count) {
+      throw new NotImplementedException();
+    }
+
+    public void WriteLine(string format, object arg0, object arg1) {
+      throw new NotImplementedException();
+    }
+
+    public void WriteLine(string format, object arg0, object arg1, object arg2) {
+      throw new NotImplementedException();
+    }
+
+    public void WriteLine(string format, object arg0, object arg1, object arg2, object arg3) {
+      throw new NotImplementedException();
+    }
+
+    public void Write(bool value) {
+      throw new NotImplementedException();
+    }
+
+    public void Write(char value) {
+      throw new NotImplementedException();
+    }
+
+    public void Write(char[] buffer) {
+      throw new NotImplementedException();
+    }
+
+    public void Write(decimal value) {
+      throw new NotImplementedException();
+    }
+
+    public void Write(double value) {
+      throw new NotImplementedException();
+    }
+
+    public void Write(int value) {
+      throw new NotImplementedException();
+    }
+
+    public void Write(long value) {
+      throw new NotImplementedException();
+    }
+
+    public void Write(object value) {
+      throw new NotImplementedException();
+    }
+
+    public void Write(float value) {
+      throw new NotImplementedException();
+    }
+
     public void Write(string value) {
       Array.ForEach(mParser.Parse(value), DoWork);
+    }
+
+    public void Write(uint value) {
+      throw new NotImplementedException();
+    }
+
+    public void Write(ulong value) {
+      throw new NotImplementedException();
+    }
+
+    public void Write(string format, object arg0) {
+      throw new NotImplementedException();
+    }
+
+    public void Write(string format, params object[] arg) {
+      throw new NotImplementedException();
+    }
+
+    public void Write(char[] buffer, int index, int count) {
+      throw new NotImplementedException();
+    }
+
+    public void Write(string format, object arg0, object arg1) {
+      throw new NotImplementedException();
+    }
+
+    public void Write(string format, object arg0, object arg1, object arg2) {
+      throw new NotImplementedException();
+    }
+
+    public void Write(string format, object arg0, object arg1, object arg2, object arg3) {
+      throw new NotImplementedException();
+    }
+
+    public string ReadLine() {
+      throw new NotImplementedException();
+    }
+
+    public ConsoleKeyInfo ReadKey() {
+      throw new NotImplementedException();
+    }
+
+    public ConsoleKeyInfo ReadKey(bool intercept) {
+      throw new NotImplementedException();
+    }
+
+    public void ResetColor() {
+      throw new NotImplementedException();
     }
 
     private void DoWork(Segment segment) {
