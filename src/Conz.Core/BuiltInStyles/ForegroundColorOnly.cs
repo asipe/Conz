dd7232b7058e81ee463430897dce50853cdc6b54
Andy Sipe <ajs.general@gmail.com>
2014-08-25 08:16:19 -0400

    added built in stylesheet with foreground colors only

---
+++ src/Conz.Core/BuiltInStyles/ForegroundColorOnly.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Conz.Core.BuiltInStyles {
+  public static class ForegroundColorOnly {
+    static ForegroundColorOnly() {
+      _Instance = new StyleSheet(null,
+                                 new Class("bl", null, ConsoleColor.Black),
+                                 new Class("db", null, ConsoleColor.DarkBlue),
+                                 new Class("dg", null, ConsoleColor.DarkGreen),
+                                 new Class("dc", null, ConsoleColor.DarkCyan),
+                                 new Class("dr", null, ConsoleColor.DarkRed),
+                                 new Class("dm", null, ConsoleColor.DarkMagenta),
+                                 new Class("dy", null, ConsoleColor.DarkYellow),
+                                 new Class("gr", null, ConsoleColor.Gray),
+                                 new Class("dgr", null, ConsoleColor.DarkGray),
+                                 new Class("b", null, ConsoleColor.Blue),
+                                 new Class("g", null, ConsoleColor.Green),
+                                 new Class("c", null, ConsoleColor.Cyan),
+                                 new Class("r", null, ConsoleColor.Red),
+                                 new Class("m", null, ConsoleColor.Magenta),
+                                 new Class("y", null, ConsoleColor.Yellow),
+                                 new Class("w", null, ConsoleColor.White));
+    }
+
+    public static StyleSheet _Instance{get;private set;}
+  }
+}
---
+++ src/Conz.UnitTests/Core/BuiltInStyles/ForegroundColorOnlyTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Conz.Core.BuiltInStyles;
+using NUnit.Framework;
+
+namespace Conz.UnitTests.Core.BuiltInStyles {
+  [TestFixture]
+  public class ForegroundColorOnlyTest : BaseTestCase {
+    [Test]
+    public void TestNamesAreUnique() {
+      var distinctNames = ForegroundColorOnly
+        ._Instance
+        .Classes
+        .Select(c => c.Name)
+        .Distinct();
+      Assert.That(ForegroundColorOnly._Instance.Classes.Length, Is.EqualTo(distinctNames.Count()));
+    }
+  }
+}
