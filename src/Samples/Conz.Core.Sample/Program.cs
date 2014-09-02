5412b98b2999679215f7b88577f32a1abd60ddc8
Andy Sipe <ajs.general@gmail.com>
2014-09-02 08:53:25 -0400

    added some additional samples

--- src/Samples/Conz.Core.Sample/Program.cs
+++ src/Samples/Conz.Core.Sample/Program.cs
@@ -17,6 +17,24 @@ namespace Conz.Core.Sample {
       Console.WriteLine();
       Console.WriteLine();
       DoFormatted();
+      Console.WriteLine();
+      Console.WriteLine();
+      Console.WriteLine();
+      MoreFormatting();
+    }
+
+    private static void MoreFormatting() {
+      const string text = "This is some sample text.  |quote|\"This is a quote\".| and this isn't a quote.\r\n|sep|-------|\r\nNote: |note|Just a sample|\r\nYes this is just a sample.\r\n|sep|-------|";
+      var conzole1 = new Conzole(new StyleSheet(new Class("default"),
+                                               new Class("quote", ConzoleColor.Default, ConzoleColor.Yellow),
+                                               new Class("note", ConzoleColor.Default, ConzoleColor.Red),
+                                               new Class("sep", ConzoleColor.White, ConzoleColor.Black, 2)));
+      var conzole2 = new Conzole(new StyleSheet(new Class("default", ConzoleColor.Blue, ConzoleColor.Yellow),
+                                               new Class("quote", ConzoleColor.Default, ConzoleColor.Cyan),
+                                               new Class("note", ConzoleColor.White, ConzoleColor.Red),
+                                               new Class("sep", ConzoleColor.Default, ConzoleColor.Red, 50)));
+      conzole1.WriteLine(text);
+      conzole2.WriteLine(text);
     }
 
     private static void DoFormatted() {
